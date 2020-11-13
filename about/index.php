60a2a64d847cd5abb9a211d50d5330b57488e5ba
RizaAdePratama <61009518+RizaAdePratama@users.noreply.github.com>
2020-11-13 17:20:23 +0700

    About Us
    Kode program dari about us

---
+++ about/index.php
@@ -0,0 +1,40 @@
+<div class="container">
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <td>
+                        <br>
+                        <h3>About</h3>                        
+                        <hr>
+                            <br>
+                            <p>Dalam era Globalisasi saat ini teknologi informasi sangat membantu meringankan pekerjaan pada semua bidang kehidupan manusia. Salah satu diantaranya adalah bidang ekonomi, karena kebutuhan dunia ekonomi demikian tinggi untuk pembangunan perekonomian. Dalam era sekarang perusahaan memiliki dua jenis pengelolaan usaha yang bergerak di bidang barang dan jasa. Perusahaan yang bergerak di bidang jasa sangatlah populer di era modern ini karena banyak sekali badan usaha yang menggunakan jasa mereka sehingga banyak pelaku usaha yang semakin dipermudah untuk mengelola barang/jasa yang dijalankan.</p>
+                            <p>Salah satu jenis jasa yang populer di masa sekarang adalah persewaan barang, perusahaan saat ini ingin mempermudah pengguna jasa mereka dengan cara membuat sistem online. Sistem online yang dibuat bertujuan untuk mempermudah badan usaha yang ingin menggunakan jasa mereka, salah satu jenis usaha yang ditawarkan adalah usaha pembelian gerobak .Untuk mempermudah orang-orang yang membutuhkan gerobak dibidang kuliner maupun bidang yang lain.</p>
+                            <br>                               
+                        <hr> 
+                        <h3>Lokasi</h3>                        
+                        <hr>
+                        <br>
+                            Jl. Saxophone Lowokwaru, Malang
+                        <br>
+                        <br>
+                        <div id="googleMap" style="width:100%;height:380px;"></div>
+                        <script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+function initialize(){
+var propertiPeta = {
+center:new google.maps.LatLng(-7.925245,112.605684), zoom:9, mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var peta = new google.maps.Map(document.getElementById("googleMap"),propertiPeta);
+
+var marker=new google.maps.Marker({
+position: new google.maps.LatLng(-7.925245,112.605684), map: peta
+});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+                        <br>
+                        <br>    
+                    </td>
+                </div>
+            </div>
+</div>
